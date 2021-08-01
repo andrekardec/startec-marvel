daeec6a22372106f01e4004fc644d11201d0d852
Andre Kardec <72898109+andrekardec@users.noreply.github.com>
2021-08-01 06:38:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">
+Marvel React App
+</h1>
+
+<h2 align="center">Startec Challenge</h2>
+
+<p align="center">Data provided by <a href="https://developer.marvel.com/" alt="Marvel's API">Marvel</a>. © 2014 Marvel</p>
+<p align="center">Deployed <a href="#">here</a>.</p>
+
+<p align="center">
+  <a href="https://github.com/andrekardec/startec-marvel/graphs/contributors">
+    <img src="https://img.shields.io/github/contributors/andrekardec/startec-marvel?color=%237159c1&logoColor=%237159c1&style=flat" alt="Contributors">
+  </a>
+  <a href="https://opensource.org/licenses/MIT">
+    <img src="https://img.shields.io/github/license/andrekardec/startec-marvel?color=%237159c1&logo=mit&license=mit" alt="License">
+  </a>
+</p>
+
+<hr>
+
+## Participants
+
+| [<img src="https://avatars.githubusercontent.com/u/72898109?v=4" width="75px;"/>](https://github.com/andrekardec) |
+| :---------------------------------------------------------------------------------------------------------------: |
+
+| [Andre Kardec](https://github.com/andrekardec)
+
+## Techs
+
+- [x] ReactJS
+- [x] React Router Dom
+- [x] Context API
+- [x] Styled Components
+- [x] TypeScript
+- [x] Lint
+- [x] Jest
+
+## Usage
+
+1. Run `npm install` or `yarn install`.<br />
+2. Run `yarn start` and access `http://localhost:3000`.<br />
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry: path.resolve(__dirname, "./src/presentation", "index.tsx"),
+    output: {
+      path: path.join(__dirname, "build"),
+      publicPath: "/",
+      filename: "index.bundle.js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: { contentBase: path.join(__dirname, "src") },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+              test: /\.svg$/,
+              use: ['@svgr/webpack'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+      alias: {
+        "@adapters": path.resolve(__dirname, "./src/adapters/"),
+        "@config": path.resolve(__dirname, "./src/config/"),
+        "@domain": path.resolve(__dirname, "./src/domain/"),
+        "@presentation": path.resolve(__dirname, "./src/presentation/"),
+      }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src/presentation/public", "index.html"),
+        }),
+        new CleanWebpackPlugin(),
+        new Dotenv()
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, './src/'),
+      port: 3000,
+      historyApiFallback: true,
+  },
+};
