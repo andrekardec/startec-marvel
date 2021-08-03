a30b1b0a53619a2f64d4c1a0e9553dc85c082313
Andre Kardec <72898109+andrekardec@users.noreply.github.com>
2021-08-03 01:51:53 -0300

    https

--- src/config/Marvel.ts
+++ src/config/Marvel.ts
@@ -1,7 +1,7 @@
 import md5 from 'md5';
 import { ICredentials } from './ICredentials';
 
-const API_MARVEL_BASE_URL = 'http://gateway.marvel.com/v1/public';
+const API_MARVEL_BASE_URL = 'https://gateway.marvel.com/v1/public';
 const API_MARVEL_PRIVATE_KEY = '4bea677559e559132a6a1b595a9c4d3837050d32';
 const API_MARVEL_PUBLIC_KEY = 'd778de677bb9a639364bbfdf40379844';
 
